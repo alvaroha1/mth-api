f6ee0e0901d4dda95d6352bdf4b3f4633ea6f85e
stevenspyramid <steven.vanasbeck@gmail.com>
2018-10-18 10:03:02 +0200

    feat: beginning of test

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,50 @@
-// TESTS GO HERE...
+const assert = require('assert');
+const chai = require('chai');
+
+const should = chai.should();
+const expect = chai.expect;
+const algorithm = '../algorithm/algorithm';
+
+describe('Array', () => {
+  describe('Does not modify input', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+
+  describe('Has the right format', () => {
+    it('have an id property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a thumbnail property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a price property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a size property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a country property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a latitude property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a longitude property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a url property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a pricePerSquareMeter property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have a estimatedPrice property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('have an estimatedPricePercentageDifference property', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
